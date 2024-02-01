a144bd7316b327d9e0cf0da9c78884319c3c79cd
Samuel1-ona <samuonanikesamuel123@gmail.com>
2024-02-02 00:21:34 +0100

    initial commit

---
+++ server/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { URL, KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: URL,
+      accounts: [`0x${KEY}`],
+    },
+  },
+};
